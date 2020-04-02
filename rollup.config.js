7241bda2511566eb97b4af877c20149621f808e1
Alfredo Bonilla <brolag@live.com>
2020-04-01 23:06:05 -0600

    Add PWA capabilities

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,11 @@
 import { createDefaultConfig } from '@open-wc/building-rollup';
+import outputManifest from 'rollup-plugin-output-manifest';
 
-// if you need to support IE11 use "modern-and-legacy-config" instead.
-// import { createCompatibilityConfig } from '@open-wc/building-rollup';
-// export default createCompatibilityConfig({ input: './index.html' });
-
-export default createDefaultConfig({ input: './index.html' });
+export default createDefaultConfig({ 
+  input: './index.html',
+  plugins: [
+    outputManifest(),
+  ] 
+});
+ 
+ 

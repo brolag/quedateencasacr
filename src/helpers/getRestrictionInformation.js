50fc77dd65360be8a3e2eac35492d71319236581
Alfredo Bonilla <brolag@live.com>
2020-04-12 22:40:38 -0600

    fix information validation

--- src/helpers/getRestrictionInformation.js
+++ src/helpers/getRestrictionInformation.js
@@ -1,31 +1,35 @@
-import { calculateWeekendRestriction, calculateWeekDayRestriction } from './calculateRestriction.js';
+import {
+  calculateWeekendRestriction,
+  calculateWeekDayRestriction,
+} from './calculateRestriction.js';
 
-const isWeekend = (day) => day === 0 || day === 6;
+const isWeekend = day => day === 0 || day === 6;
 
-export const getRestrictionInfo = plateNumber =>  {
-	const week = [ 'Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado']
-	const noRestriction = []; const regular = []; const full = [];
+export const getRestrictionInfo = plateNumber => {
+  const week = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+  const noRestriction = [];
+  const regular = [];
+  const full = [];
 
-	week.forEach((day, index) => {
-		if (isWeekend(index)) {
-			if(!calculateWeekendRestriction(index, plateNumber)) {
-				regular.push(day)
-			}
-			else {
-				full.push(day)
-			}
-		}
-		else if (!isWeekend(index) && calculateWeekDayRestriction(index, plateNumber)) {
-			full.push(day)
-		}
-		else {
-			noRestriction.push(day)
-		}
-	})
+  week.forEach((day, index) => {
+    if (isWeekend(index)) {
+      if (!calculateWeekendRestriction(index, plateNumber)) {
+        regular.push(day);
+      } else {
+        full.push(day);
+      }
+    } else if (!isWeekend(index) && calculateWeekDayRestriction(index, plateNumber)) {
+      full.push(day);
+    } else {
+      noRestriction.push(day);
+    }
+  });
 
-	return plateNumber ? {
-		noRestriction,
-		regular,
-		full
-	} : null
-}
+  return full.length > 1
+    ? {
+        noRestriction,
+        regular,
+        full,
+      }
+    : null;
+};
